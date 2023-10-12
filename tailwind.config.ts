d0f2739956106b08f89b3d37168cdee29897ba77
amabelleS <amabelletr@gmail.com>
2023-10-13 01:06:33 +0300

    Cleaned styling, config tailwind, +Home page title

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,23 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
     },
   },
   plugins: [],
