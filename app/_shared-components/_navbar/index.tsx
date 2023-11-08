e1b3df450bd3bcd654a91d7292d36032cd86f960
amabelleS <amabelletr@gmail.com>
2023-11-08 22:00:32 +0200

    starter nav & links

--- app/_shared-components/_navbar/index.tsx
+++ app/_shared-components/_navbar/index.tsx
@@ -1,31 +1,30 @@
 import Link from 'next/link';
-import NavLinks from './nav-links';
-// import AcmeLogo from '@/app/ui/acme-logo';
-import { useSession, signIn, signOut } from "next-auth/react"
+import NavLinks, {signOut} from './nav-links';
+import { redirect } from 'next/navigation'
 
 
-const LoginComponent = () => {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-}
+// const LoginComponent = () => {
+//   const { data: session } = useSession()
+//   if (session) {
+//     return (
+//       <>
+//         Signed in as {session.user?.email} <br />
+//         <button onClick={() => signOut()}>Sign out</button>
+//       </>
+//     )
+//   }
+//   return (
+//     <>
+//       Not signed in <br />
+//       <button onClick={() => signIn()}>Sign in</button>
+//     </>
+//   )
+// }
 
 type Props = {}
 
 const Navbar = (props: Props) => {
-  const { data: session, status } = useSession()
+  // const { data: session, status } = useSession()
 
   // if (status === "authenticated") {
   //   return <p>Signed in as {session.user.email}</p>
@@ -33,33 +32,23 @@ const Navbar = (props: Props) => {
 
   // return <a href="/api/auth/signin">Sign in</a>
   return (
-    <div>
-      <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
-      >
-        {/* <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
-        </div> */}
-      </Link>
+    <nav className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form
+        {/* <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div> */}
+        {/* <form
           action={async () => {
             'use server';
-            await signOut();
+            // await signOut();
+            redirect('/api/auth/signout')
           }}
         >
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            {/* <PowerIcon className="w-6" /> */}
-            <div className="hidden md:block">Sign Out</div>
+          <div className="hidden md:block">Sign Out</div>
           </button>
-        </form>
+        </form> */}
       </div>
-    </div>
-    </div>
+    </nav>
   )
 }
 
--- app/_shared-components/_navbar/nav-links.tsx
+++ app/_shared-components/_navbar/nav-links.tsx
@@ -4,46 +4,49 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
-} from '@heroicons/react/24';
+} from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useSession, signIn, signOut } from "next-auth/react"
+
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+    { name: 'Home', href: '/', icon: HomeIcon },
+    {
+        name: 'Community',
+        href: '/error',
+        icon: UserGroupIcon,
+    },
 ];
 
 export default function NavLinks() {
   const pathname = usePathname();
 
   return (
-    <>
+      <ul className="hidden space-x-2 md:h-auto md:flex gap-8">
       {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-              {
-                'bg-sky-100 text-blue-600': pathname === link.href,
-              },
-            )}
-          >
+          const LinkIcon = link.icon;
+          return (
+              <Link
+              key={link.name}
+              href={link.href}
+              className={clsx(
+                  'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                  {
+                      'bg-sky-100 text-blue-600': pathname === link.href,
+                    },
+                    )}
+                    >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
         );
-      })}
-    </>
+    })}
+    </ul>
   );
 }
+
+export {signOut}
