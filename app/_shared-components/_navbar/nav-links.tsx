8b683bd5b2df9c0508868cbb1bf64f861894e456
amabelleS <amabelletr@gmail.com>
2023-11-09 03:21:59 +0200

    removed unused import

--- app/_shared-components/_navbar/nav-links.tsx
+++ app/_shared-components/_navbar/nav-links.tsx
@@ -3,7 +3,6 @@
 import {
   UserGroupIcon,
   HomeIcon,
-  DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
