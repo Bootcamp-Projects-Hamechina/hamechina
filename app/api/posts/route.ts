01b2b3afe2615d8d1666c1c00b07c3cd8482fab0
amabelleS <amabelletr@gmail.com>
2023-12-14 22:06:27 +0200

    Added prisma mongodb connection

---
+++ app/api/posts/route.ts
@@ -0,0 +1,5 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default function handler(req: NextRequest, res: NextResponse) {
+    // res.status(200).json({ message: 'Hello from the /api/posts endpoint!' });
+  }
