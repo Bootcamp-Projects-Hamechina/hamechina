642c928ade9185ac45dd61630b58d8b2150daa91
amabelleS <amabelletr@gmail.com>
2023-12-15 14:06:49 +0200

    Google authorization, deployment bug

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -14,7 +14,14 @@ export const options: NextAuthOptions = {
         }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: {
+                params: {
+                  prompt: "consent",
+                  access_type: "offline",
+                  response_type: "code"
+                }
+            }
           }),
         CredentialsProvider({
             name: "Credentials",
